39542d4e427e7478f8fdde0c10fd6339985bcecc
Amal Altibaev <maroon775@gmail.com>
2020-05-25 18:18:53 +0300

    Update example in public dir

--- public/js/script.js
+++ public/js/script.js
@@ -16,11 +16,29 @@ document.addEventListener('readystatechange', function(event) {
         document.querySelector('#addPosition').addEventListener('click', function(){
             stickyNotesInstance.createSticker({
                 contentComponent: new StickyNotes.Components.content.Textarea({
-                    resizable: true
+                    resizable: false
                 }),
                 position: {
                     left:100,
                     top: 200
+                }
+            })
+        });
+        
+        document.querySelector('#addAdvanced').addEventListener('click', function(){
+            stickyNotesInstance.createSticker({
+                contentComponent: new StickyNotes.Components.content.Textarea({
+                    resizable: true, // optional
+                    
+                    maxWidth: 400, // optional
+                    maxHeight: 400, // optional
+                
+                    minWidth: 200, // optional & as initial width
+                    minHeight: 200, // optional & as initial height
+                }),
+                position: {
+                    left:300, // optional
+                    top: 300 // optional
                 }
             })
         });
